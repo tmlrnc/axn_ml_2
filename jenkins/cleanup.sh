e97b197d319ce43ac9331698dbe9ac2c18f20efd
Joseph Collard <jcollard@users.noreply.github.com>
2020-07-31 14:31:35 -0400

    Jcollard/prune docs (#13)
    Removed prune step from cleanup since Jenkins needs username and password to run git fetch.

--- jenkins/cleanup.sh
+++ jenkins/cleanup.sh
@@ -3,7 +3,3 @@ set -e
 set -x
 ENV_PATH=".python_env"
 source .python_env/bin/activate
-if [[ "$BRANCH_NAME" == *"develop"* ]]; then
-    echo "Merged into develop, pruning buckets..."
-    bash utils/doc.sh prune
-fi
