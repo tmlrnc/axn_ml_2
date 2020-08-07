76199518ae027fd37c20686075c87edaac17da52
Tom Lorenc <tmlnrc@gmail.com>
2020-08-07 16:49:31 -0400

    final predict model docs

--- axn/ml/predict/predictor/algorithm/random_forest.py
+++ axn/ml/predict/predictor/algorithm/random_forest.py
@@ -1,3 +1,56 @@
+"""
+Random forest
+
+<img src="images/rf.png" alt="OHE">
+
+like its name implies, consists of a large number of individual decision trees
+that operate as an ensemble. Each individual tree in the random forest spits out a
+class prediction and the class with the most votes becomes our model’s prediction
+A large number of relatively uncorrelated models (trees) operating as a
+committee will outperform any of the individual constituent models.
+
+The low correlation between models is the key. Just like how investments with low correlations
+(like stocks and bonds) come together to form a portfolio that is greater than the sum of its parts,
+uncorrelated models can produce ensemble predictions that are more accurate than any of the
+individual predictions. The reason for this wonderful effect is that the trees protect each other
+from their individual errors (as long as they don’t constantly all err in the same direction).
+While some trees may be wrong, many other trees will be right, so as a group the trees are able to
+move in the correct direction. So the prerequisites for random forest to perform well are:
+
+There needs to be some actual signal in our features so that models built using those features do better than random guessing.
+The predictions (and therefore the errors) made by the individual trees need to have low correlations with each other.
+
+
+Parameters
+----------
+X_test: array
+ testing features
+
+X_train: array
+ training features
+
+y_test: array
+ testing label
+
+y_train: array
+ testing label
+
+Returns:
+----------
+ target: array - label to be predicted or classified
+
+
+     trains the scikit-learn  python machine learning algorithm library function
+     https://scikit-learn.org
+
+     then passes the trained algorithm the features set and returns the
+     predicted y test values form, the function
+
+     then compares the y_test values from scikit-learn predicted to
+     y_test values passed in
+
+     then returns the accuracy
+     """
 from sklearn.ensemble import RandomForestClassifier
 
 from axn.ml.predict.predictor import OneHotPredictor, Commandline
@@ -22,6 +75,45 @@ class RandomForest_OHP(OneHotPredictor):
 
     def predict(self):
         """
+Random forest
+
+like its name implies, consists of a large number of individual decision trees
+that operate as an ensemble. Each individual tree in the random forest spits out a
+class prediction and the class with the most votes becomes our model’s prediction
+A large number of relatively uncorrelated models (trees) operating as a
+committee will outperform any of the individual constituent models.
+
+The low correlation between models is the key. Just like how investments with low correlations
+(like stocks and bonds) come together to form a portfolio that is greater than the sum of its parts,
+uncorrelated models can produce ensemble predictions that are more accurate than any of the
+individual predictions. The reason for this wonderful effect is that the trees protect each other
+from their individual errors (as long as they don’t constantly all err in the same direction).
+While some trees may be wrong, many other trees will be right, so as a group the trees are able to
+move in the correct direction. So the prerequisites for random forest to perform well are:
+
+There needs to be some actual signal in our features so that models built using those features do better than random guessing.
+The predictions (and therefore the errors) made by the individual trees need to have low correlations with each other.
+
+
+ Parameters
+----------
+X_test: array
+    testing features
+
+X_train: array
+    training features
+
+y_test: array
+    testing label
+
+y_train: array
+    testing label
+
+Returns:
+----------
+    target: array - label to be predicted or classified
+
+
         trains the scikit-learn  python machine learning algorithm library function
         https://scikit-learn.org
 
